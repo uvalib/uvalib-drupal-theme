122d1d0c8b8961cf63d9c8d73895815a68dac030
Christopher <cmw5mc@virginia.edu>
2022-10-07 10:19:56 -0400

    deployed new regions and javascript hook

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,14 +1,12 @@
+"use strict";
+
 /**
  * @file
  * This file is to add any custom js for the drupal8 w3css sub-theme.
  */
-
 (function ($) {
-
   'use strict';
 
-  Drupal.behaviors.d8w3cssSubthemeBehavior = {
-      // Perform jQuery as normal in here.
+  Drupal.behaviors.d8w3cssSubthemeBehavior = {// Perform jQuery as normal in here.
   };
-
 })(jQuery);
