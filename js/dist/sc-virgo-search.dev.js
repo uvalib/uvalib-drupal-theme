a1067b56aa5fbcb18155d53c56b66a4e884d2771
Christopher <cmw5mc@virginia.edu>
2022-11-25 20:34:23 -0500

    special collections

---
+++ js/dist/sc-virgo-search.dev.js
@@ -0,0 +1,7 @@
+// (function searchVirgo4() {
+//   var query = encodeURIComponent(document.getElementById("scQuery").value);
+//   var url = `https://search.lib.virginia.edu/?mode=advanced&q=keyword:%20{${query}}&pool=uva_library&filter=%7B%22FilterLibrary%22%3A%5B%22Special%20Collections%22%5D%7D`;
+//   location = url;
+//   return false;
+// })(jQuery);
+"use strict";
