4de162bdb80e1c0487c45876b02b75298c2f8c12
Christopher <cmw5mc@virginia.edu>
2022-12-02 16:36:16 -0500

    libra

--- js/sc-virgo-search.js
+++ js/sc-virgo-search.js
@@ -4,3 +4,10 @@ function searchVirgo4() {
   location = url;
   return false;
 };
+
+function searchVirgo4Libra() {
+    var query = encodeURIComponent(document.getElementById("libraQuery").value);
+    var url = `https://search.lib.virginia.edu/?mode=advanced&q=keyword:%20{${query}}&pool=uva_library&filter=%7B%22FilterDigitalCollection%22%3A%5B%22Libra%20Data%20Repository%22%2C%22Libra%20ETD%20Repository%22%2C%22Libra%20Open%20Repository%22%5D%7D`;
+    location = url;
+    return false;
+};
