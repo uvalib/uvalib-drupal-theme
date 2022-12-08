229b69b4e50446f4e2ac4c1a9691abc368f1634a
Christopher <cmw5mc@virginia.edu>
2022-12-08 08:55:25 -0500

    new main menu and header

--- js/getdate.js
+++ js/getdate.js
@@ -1,8 +1,29 @@
 (function ($) {
-  "use strict";
+	"use strict";
 
-    var options = {  weekday: 'long', month: 'long', day: 'numeric' };
-    var prnDt = ' ' + new Date().toLocaleDateString('en-us', options);
-    $('#homehoursdate').html(prnDt);
-
+	var options = { weekday: "long", month: "long", day: "numeric" };
+	var prnDt = " " + new Date().toLocaleDateString("en-us", options);
+	$("#homehoursdate").html(prnDt);
 })(jQuery);
+
+/*{ 
+  EVENTS STYLING FOR DATE
+  <div>
+    <span id="homehoursMon"></span><br>
+    <span id="homehoursDay"></span><br> 
+    <span id="homehoursWeekDay"></span>
+</div>
+
+
+(function ($) {
+	"use strict";
+	var optionswkday = { weekday: "short" };
+	var optionsmonth = { month: "short" };
+	var optionsday = { day: "numeric" };
+	var prnDay = " " + new Date().toLocaleDateString("en-us", optionsday);
+	var prnMon = " " + new Date().toLocaleDateString("en-us", optionsmonth);
+	var prnWkDay = " " + new Date().toLocaleDateString("en-us", optionswkday);
+	$("#homehoursDay").html(prnDay);
+	$("#homehoursMon").html(prnMon);
+	$("#homehoursWeekDay").html(prnWkDay);
+})(jQuery); }*/
