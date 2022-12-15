1cfb2cdf53d26edbbca9a347dc3062b2dbf19163
Christopher <cmw5mc@virginia.edu>
2022-12-15 12:10:35 -0500

    mobile nav-fixed

--- js/mobile-menu-v2.js
+++ js/mobile-menu-v2.js
@@ -2,78 +2,27 @@
 // hamburger menu located in page.html.twig line 102 - #openmobilemenu
 // close menu located in page.html.twig line 70 - #close-nav
 
-document
-	.getElementById("openmobilemenu")
-	.addEventListener(
-		"click",
-		(e) =>
-			(document.getElementById("main-navigation-vTEST").style.display = "block")
-	);
-undefined;
-
-document
-	.getElementById("close-nav")
-	.addEventListener(
-		"click",
-		(e) =>
-			(document.getElementById("main-navigation-vTEST").style.display = "none")
-	);
-
-// const targetDivOpen = document.getElementById("main-navigation-vTEST");
-// const btnOpen = document.querySelectorAll("#openmobilemenu");
-// btnOpen.onclick = function () {
-// 	if (targetDivOpen.style.display !== "none") {
-// 		targetDivOpen.style.display = "none";
-// 	} else {
-// 		targetDivOpen.style.display = "block";
-// 	}
-// };
-
-// const targetDivClose = document.getElementById("main-navigation-vTEST");
-// const btnClose = document.querySelectorAll("#close-nav");
-// btnClose.onclick = function () {
-// 	if (targetDivClose.style.display !== "none") {
-// 		targetDivClose.style.display = "none";
-// 	} else {
-// 		targetDivClose.style.display = "block";
-// 	}
-// };
-
-// var isDialogSupported = true;
-// if (!window.HTMLDialogElement) {
-// 	document.body.classList.add("no-dialog");
-// 	isDialogSupported = false;
-// }
-
-// openmobilemenu.onclick = () => {
-// 	// if (isDialogSupported) {
-// 	// mobilemenumodal.showModal();
-// 	// } else {
-// 	mobilemenumodal.setAttribute("open", "");
-// 	// }
-// 	//   Focus first input when dialog opens
-// 	mobilemenumodal.querySelector(".li-0").focus();
-// };
-
-// mobilemenumodal.addEventListener("transitionend", (e) => {
-// 	mobilemenumodal.querySelector(".li-0").focus();
+// document.addEventListener("DOMContentLoaded", (event) => {
+// 	// OPEN
+// 	document
+// 		.getElementById("openmobilemenu")
+// 		.addEventListener("click", (e) =>
+// 			window.openDialog("main-navigation-vTEST", "openmobilemenu")
+// 		);
+// 	// CLOSE
+// 	document.getElementById("close-nav").addEventListener("click", (e) => {
+// 		console.log("foo-is-here");
+// 		window.closeDialog();
+// 	});
 // });
 
-// const showButton = document.getElementById("openmobilemenu");
-// const mobileDialog = document.getElementById("main-navigation-v");
-// // const outputBox = document.querySelector("output");
-// const selectEl = mobileDialog.querySelector(".li-item-mobile-main-nav");
-// const closeBtn = mobileDialog.querySelector("#close-nav");
+document.addEventListener("DOMContentLoaded", (event) => {
+	document
+		.getElementById("openmobilemenu")
+		.addEventListener("click", (e) =>
+			window.openDialog("main-navigation-vTEST", "openmobilemenu")
+		);
 
-// // "Update details" button opens the <dialog> modally
-// showButton.addEventListener("click", () => {
-// 	mobileDialog.showModal();
-// });
-// // "Favorite animal" input sets the value of the submit button
-// selectEl.addEventListener("change", (e) => {
-// 	closeBtn.value = selectEl.value;
-// });
-// // "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
-// mobileDialog.addEventListener("close", () => {
-// 	outputBox.value = `ReturnValue: ${mobileDialog.returnValue}.`;
-// });
+	const closeNav = document.getElementById("close-nav");
+	closeNav.addEventListener("click", (e) => window.closeDialog(closeNav));
+});
