da371d668ec3ba66cb4a8b2b5a6592818831ecf1
Christopher <cmw5mc@virginia.edu>
2023-01-17 15:21:25 -0500

    setup for custom block styling

--- js/getdate.js
+++ js/getdate.js
@@ -11,22 +11,22 @@
 
 /*{ 
   EVENTS STYLING FOR DATE
-  <div>
-    <span id="homehoursMonth"></span><br>
-    <span id="homehoursDay"></span><br> 
+  <p class=“event-date”>
+    <span id="homehoursMonth"></span>
+    <span id="homehoursDay"></span> 
     <span id="homehoursWeekDay"></span>
-	</div>
+	</p>
+*/
 
-
-(function ($) {
-	"use strict";
-	var optionswkday = { weekday: "short" };
-	var optionsmonth = { month: "short" };
-	var optionsday = { day: "numeric" };
-	var prnDay = " " + new Date().toLocaleDateString("en-us", optionsday);
-	var prnMon = " " + new Date().toLocaleDateString("en-us", optionsmonth);
-	var prnWkDay = " " + new Date().toLocaleDateString("en-us", optionswkday);
-	$("#homehoursDay").html(prnDay);
-	$("#homehoursMonth").html(prnMon);
-	$("#homehoursWeekDay").html(prnWkDay);
-})(jQuery); }*/
+// (function ($) {
+// 	"use strict";
+// 	var optionswkday = { weekday: "short" };
+// 	var optionsmonth = { month: "short" };
+// 	var optionsday = { day: "numeric" };
+// 	var prnDay = new Date().toLocaleDateString("en-us", optionsday) + ",";
+// 	var prnMonth = " " + new Date().toLocaleDateString("en-us", optionsmonth);
+// 	var prnWkDay = " " + new Date().toLocaleDateString("en-us", optionswkday);
+// 	$("#homehoursDay").html(prnDay);
+// 	$("#homehoursMonth").html(prnMonth);
+// 	$("#homehoursWeekDay").html(prnWkDay);
+// })(jQuery);
