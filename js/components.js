1657902854930c09d1e17910a517b26b8e0bf238
Christopher <cmw5mc@virginia.edu>
2023-05-27 10:54:18 -0400

    testing "today"

--- js/components.js
+++ js/components.js
@@ -1,4 +1,30 @@
-//TOGGLE BUTTON - switching classes
+// //TOGGLE BUTTON - switching classes
+// // Wait for the DOM to load
+// document.addEventListener("DOMContentLoaded", function () {
+// 	// Get the checkbox input element
+// 	var checkbox = document.querySelector('.toggleSwitch input[type="checkbox"]');
+// 	// Get the list-view and grid-view elements
+// 	var listView = document.querySelector(".list-view");
+// 	var gridView = document.querySelector(".grid-view");
+
+// 	// Set the initial state (grid-view active, list-view hidden)
+// 	listView.style.display = "block";
+// 	gridView.style.display = "none";
+
+// 	// Add an event listener for the change event on the checkbox
+// 	checkbox.addEventListener("change", function () {
+// 		if (checkbox.checked) {
+// 			// Hours selected, show list-view and hide grid-view
+// 			listView.style.display = "block";
+// 			gridView.style.display = "none";
+// 		} else {
+// 			// Location selected, show grid-view and hide list-view
+// 			listView.style.display = "none";
+// 			gridView.style.display = "block";
+// 		}
+// 	});
+// });
+
 // Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", function () {
 	// Get the checkbox input element
@@ -6,21 +32,41 @@ document.addEventListener("DOMContentLoaded", function () {
 	// Get the list-view and grid-view elements
 	var listView = document.querySelector(".list-view");
 	var gridView = document.querySelector(".grid-view");
+	var toggleButton = document.querySelector(".toggleSwitch a");
 
 	// Set the initial state (grid-view active, list-view hidden)
 	listView.style.display = "block";
 	gridView.style.display = "none";
 
-	// Add an event listener for the change event on the checkbox
-	checkbox.addEventListener("change", function () {
+	// Function to handle the toggle state
+	function toggleState() {
 		if (checkbox.checked) {
 			// Hours selected, show list-view and hide grid-view
 			listView.style.display = "block";
 			gridView.style.display = "none";
+			toggleButton.setAttribute("aria-pressed", "true");
 		} else {
 			// Location selected, show grid-view and hide list-view
 			listView.style.display = "none";
 			gridView.style.display = "block";
+			toggleButton.setAttribute("aria-pressed", "false");
+		}
+	}
+
+	// Add an event listener for the change event on the checkbox
+	checkbox.addEventListener("change", toggleState);
+
+	// Add an event listener for the click event on the toggle button
+	toggleButton.addEventListener("click", function () {
+		checkbox.checked = !checkbox.checked;
+		toggleState();
+	});
+
+	// Add keyboard support for the toggle button
+	toggleButton.addEventListener("keydown", function (event) {
+		if (event.code === "Enter" || event.code === "Space") {
+			checkbox.checked = !checkbox.checked;
+			toggleState();
 		}
 	});
 });
